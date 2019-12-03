3f09076a41988c5cbab6d47bb64635bf693ef9e3
stevevenzearo <2226069315@qq.com>
2019-12-03 22:55:35 +0800

    access decorator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,8 +2,8 @@
 
 namespace ConsoleApp1 {
     class Program {
-        const int a = 0;
-        private static int b = a;
+        private const int A = 0;
+        private static int _b = A;
         static void Main() {
             Rectangle r = new Rectangle();
             r.AcceptDetails(3.23, 4.25);
@@ -12,7 +12,8 @@ namespace ConsoleApp1 {
             Console.WriteLine(s2);
             const double d = 2E2;
             Console.WriteLine(d);
-            Console.ReadLine();
+            Circle circle = new Circle(1);
+            Console.WriteLine(circle ?? new Circle(3));
         }
     }
 }
