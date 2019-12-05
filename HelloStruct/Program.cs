602df667b9310b1df4f71241359bbd5a19c0bda4
steve <stevenzearo@gmail.com>
2019-12-05 10:48:47 +0800

    struct

---
+++ HelloStruct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloStruct {
+    static class Program {
+        static void Main(string[] args) {
+            Student student = new Student("steve", 23, 86.7);
+            Process(student);
+            Console.WriteLine(student);
+        }
+
+        static void Process(Student student) {
+            student.Age = 22;
+        }
+    }
+}
